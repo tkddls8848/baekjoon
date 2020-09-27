1067ea33bdf73dec0069170450f7faa5ed52c215
PSI <tkddls8848@gmail.com>
2020-09-27 12:13:27 +0900

    No.10870

---
+++ 10870.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+
+def fibonacci(N):
+    if N == 0:
+        return 0
+    elif N <= 2:
+        return 1
+    else:
+        return fibonacci(N - 1) + fibonacci(N - 2)
+
+
+print(fibonacci(N))
