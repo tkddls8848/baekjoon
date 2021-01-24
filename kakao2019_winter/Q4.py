cc0c111abdfd27a42352d90715aad5dc5cae76e6
PSI <tkddls8848@gmail.com>
2021-01-24 20:31:14 +0900

    kakao 2019_winter intern Q4 (solved by dictionary)

--- kakao2019_winter/Q4.py
+++ kakao2019_winter/Q4.py
@@ -1,25 +1,24 @@
-k = 10
+k = 100
 room_number = [1,3,4,1,3,1]
+
 
 def solution(k, room_number):
     answer = []
-    room = [False for i in range(k)]
-    print(room)
-    for i in range(len(room_number)):
-        print(room_number[i])
-        if not room[room_number[i]]:
-            room[room_number[i]] = True
-            answer.append(room_number[i])
-        else:
-            print('occupy')
-            cnt = room_number[i]
-            while room[cnt]:
-                cnt += 1
-            print('cnt',cnt)
-            room[cnt] = True
-            answer.append(cnt)
-    print(room)
-    print(answer)
+    checklist = {}
+
+    def check_avail_room(room):
+        tmp = []
+        while checklist.get(room) is not None:
+            tmp.append(checklist.get(room))
+            room = checklist.get(room)
+        print(tmp,checklist)
+        for t in tmp:
+            checklist[t] = room + 1
+        checklist[room] = room + 1
+        return room
+    for r in room_number:
+        answer.append(check_avail_room(r))
+
     return answer
 
-solution(k, room_number)
+print(solution(k, room_number))
